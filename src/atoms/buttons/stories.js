f3706a95e8fcaf3e96f2d03a512f7195cfc57c0d
Matt Travi <programmer@travi.org>
2020-01-04 20:57:40 -0600

    feat(components): exposed components through the public api

--- src/atoms/buttons/stories.js
+++ src/atoms/buttons/stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Send} from '@material-ui/icons';
 import {storiesOf} from '@storybook/react';
 import any from '@travi/any';
-import PrimaryButton from './primary';
+import {PrimaryButton} from '../..';
 
 storiesOf('Atoms/Buttons/Primary', module)
   .add('default', () => (<PrimaryButton>{any.word()}</PrimaryButton>))
--- src/atoms/form/text-input/stories.js
+++ src/atoms/form/text-input/stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import any from '@travi/any';
-import TextInput from '.';
+import {TextInput} from '../../..';
 
 storiesOf('Atoms/Form/Text-Input', module).add('default', () => (
   <TextInput id={any.word()} label={any.word()} />
