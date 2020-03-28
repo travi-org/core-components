adaa894826d8b63b4e1cb2f2b8879425560f1105
Matt Travi <programmer@travi.org>
2020-03-28 13:36:08 -0500

    feat(input-variant): added a full-wdith variant
    for inputs that should fill the width of their containers

--- src/atoms/form/text-input/presentational.js
+++ src/atoms/form/text-input/presentational.js
@@ -2,15 +2,25 @@ import React from 'react';
 import {bool, string} from 'prop-types';
 import {TextField} from '@material-ui/core';
 
-export default function Input({id, label, type, multiline}) {
-  return <TextField id={id} name={id} label={label} type={type} multiline={multiline} />;
+export default function Input({id, label, type, multiline, variant}) {
+  return (
+    <TextField
+      id={id}
+      name={id}
+      label={label}
+      type={type}
+      multiline={multiline}
+      css={{...'full-width' === variant && {width: '100%'}}}
+    />
+  );
 }
 
 Input.propTypes = {
   id: string.isRequired,
   label: string.isRequired,
   type: string,
-  multiline: bool
+  multiline: bool,
+  variant: string
 };
 
 Input.defaultProps = {
--- src/atoms/form/text-input/stories.js
+++ src/atoms/form/text-input/stories.js
@@ -3,6 +3,10 @@ import {storiesOf} from '@storybook/react';
 import any from '@travi/any';
 import {TextInput} from '../../..';
 
-storiesOf('Atoms/Form/Text-Input', module).add('default', () => (
-  <TextInput id={any.word()} label={any.word()} />
-));
+storiesOf('Atoms/Form/Text-Input', module)
+  .add('default', () => <TextInput id={any.word()} label={any.word()} />)
+  .add('full-width', () => (
+    <div css={{maxWidth: 500}}>
+      <TextInput id={any.word()} label={any.word()} variant="full-width" />
+    </div>
+  ));
