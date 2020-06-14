3e884b9ce70e52f9f4c01e41064326cb27fc5188
Matt Travi <programmer@travi.org>
2020-06-14 17:25:46 -0500

    docs(any): seeded `@travi/any` in the webpack config
    since the environment variable attempts were not working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,10 @@
+const webpack = require('webpack');
+
 module.exports = {
   stories: ['../src/**/stories.js'],
+  webpackFinal: config => {
+    config.plugins.push(new webpack.DefinePlugin({'process.env': {ANY_SEED: JSON.stringify(0.4070123094134033)}}));
+
+    return config;
+  }
 };
